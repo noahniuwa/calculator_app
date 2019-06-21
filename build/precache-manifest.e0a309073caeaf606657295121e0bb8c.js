f3642549f14525125ca220018c57be21969f828c
Noah Maizels <noahmaizels@Noah.local>
2019-06-22 00:17:46 +0800

    change api call time

--- build/precache-manifest.e0a309073caeaf606657295121e0bb8c.js
+++ build/precache-manifest.e0a309073caeaf606657295121e0bb8c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "86e1cb2ba99209917bf36451e138c43f",
+    "revision": "ad7a733bb2543617282d88c911e9343f",
     "url": "/index.html"
   },
   {
-    "revision": "7aaec90e947b951e0ddc",
+    "revision": "8233e22af5c8d3e3bb8d",
     "url": "/static/css/main.a9658ca8.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.3a5ea9cb.chunk.js"
   },
   {
-    "revision": "7aaec90e947b951e0ddc",
-    "url": "/static/js/main.c11497bd.chunk.js"
+    "revision": "8233e22af5c8d3e3bb8d",
+    "url": "/static/js/main.f2d16d58.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f09fa13d815813f781d79260bee948e5.js"
+  "/precache-manifest.e0a309073caeaf606657295121e0bb8c.js"
 );
 
 self.addEventListener('message', (event) => {
